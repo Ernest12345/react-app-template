10cb88e5b1d19094317b79c056146ccc56efa8df
Ernest <ernest1637@gmail.com>
2021-05-10 13:50:33 +0600

    Connected and configured axios

---
+++ src/API/index.js
@@ -0,0 +1,18 @@
+import Axios from 'axios';
+
+const instance = Axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const authToken = ""
+    if (authToken) {
+      config.headers.Authorization = `Token ${authToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default instance;
