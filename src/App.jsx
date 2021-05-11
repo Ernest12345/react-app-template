d953e6dad05eeebe412d18522d1dccfada41cc2b
Ernest <ernest1637@gmail.com>
2021-05-11 11:18:43 +0600

    Updated styles for spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+/*eslint-disable*/
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" />
-      </Switch>
-    </Router>
+    <div>
+      <div className={"mb-50"} style={{background:"red",width:"200px",height:"200px"}}>
+        <p>42242</p>
+      </div>
+      <div style={{background:"blue",width:"200px",height:"200px"}}/>
+      <div style={{background:"green",width:"200px",height:"200px"}}/>
+    </div>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" />
+    //   </Switch>
+    // </Router>
   );
 }
 export default App;
