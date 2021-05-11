4cf2aced14361ebb35a3af7eabe94a688f39e9ff
Ernest <ernest1637@gmail.com>
2021-05-11 11:20:42 +0600

    deleted test items

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-/*eslint-disable*/
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <div className={"mb-50"} style={{background:"red",width:"200px",height:"200px"}}>
-        <p>42242</p>
-      </div>
-      <div style={{background:"blue",width:"200px",height:"200px"}}/>
-      <div style={{background:"green",width:"200px",height:"200px"}}/>
-    </div>
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" />
-    //   </Switch>
-    // </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/" />
+      </Switch>
+    </Router>
   );
 }
 export default App;
