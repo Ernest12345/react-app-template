fe38a37db04dc776b63f4d87913a56384fce1070
Ernest <ernest1637@gmail.com>
2021-05-20 12:44:16 +0600

    Configured absolute imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './store';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './styles/globals.scss';
 
